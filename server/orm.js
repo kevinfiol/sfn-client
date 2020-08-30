7b0044adebd6b26d5f37af84cbe425da7cc58e6c
kevinfiol <me@kevinfiol.com>
2020-08-30 00:31:48 -0400

    MAJOR: This repository now only contains the client code. see sfn-server for server

--- server/orm.js
+++
@@ -1,8 +0,0 @@
-const { Model } = require('objection');
-const Knex = require('knex');
-const config = require('./config.js');
-
-const knex = Knex(config.database);
-Model.knex(knex);
-
-module.exports = { Model, knex };
--- server/run.js
+++
@@ -1,4 +0,0 @@
-const config = require('./config.js');
-const app = require('./src/app.js');
-
-app.listen(config.port, () => console.log(`Listening on ${config.port}...`));
