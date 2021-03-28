9ed8d8808cc0b86452881e067b34b33c1e0b3125
kevinfiol <me@kevinfiol.com>
2021-03-28 13:27:09 -0400

    update server url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     services: {
         sfn_server: {
             // SFN_SERVER_URL: 'http://localhost:8080'
-            SFN_SERVER_URL: 'https://sfn-server.herokuapp.com'
+            SFN_SERVER_URL: 'https://sfn-server.sheev.net'
         }
     }
 }
